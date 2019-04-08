dcc179aa3ab8d9f59058b17d5d5771aa62b1262a
cam39porter <cam.b.porter@gmail.com>
2019-04-08 10:56:36 -0400

    font

--- src/components/CompanyLogo.js
+++ src/components/CompanyLogo.js
@@ -36,7 +36,7 @@ class CompanyLogo extends React.Component {
             }`}
           >
             {isMouseOver ? (
-              <div className={`pt3 tc b light-blue`}>{company.name}</div>
+              <div className={`pt3 tc b light-blue f6`}>{company.name}</div>
             ) : (
               <img
                 style={{
